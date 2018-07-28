dd3913db2ce3b3806608f90a11a2f25849344264
Dave Briccetti <daveb@davebsoft.com>
2018-07-27 22:13:06 -0700

    Use arcade physics for the ball bouncing

--- bounce/game.js
+++ bounce/game.js
@@ -1,29 +1,20 @@
 window.onload = () => {
     class MainScene extends Phaser.Scene {
-        init() {
-            this.frame = 0;
-            this.dx = 3;
-        }
-
         preload() {
             this.load.image("ball", "ball.png");
         }
 
         create() {
-            this.ball = this.add.sprite(game.config.width / 2, game.config.height / 2, "ball");
-            const ballScale = 0.25;
-            this.ball.setScale(ballScale);
-            this.scaledBallHalfWidth = (this.ball.width / 2) * ballScale;
+            const ball = this.ball = this.physics.add.sprite(game.config.width / 2, 50, "ball");
+            ball.setBounce(0.95);
+            ball.setCollideWorldBounds(true);
+            ball.setVelocity(100, 0);
+            ball.body.drag.set(4);
+            ball.setScale(0.25);
         }
 
         update() {
-            const h = game.config.height * 0.9;
-            this.ball.y = 40 + h - Math.abs(Math.cos(++this.frame / 30)) * h;
-            this.ball.x += this.dx;
-            this.ball.angle += this.dx * 2;
-            if (this.ball.x < this.scaledBallHalfWidth || this.ball.x > game.config.width - this.scaledBallHalfWidth) {
-                this.dx *= -1;
-            }
+            this.ball.setAngularVelocity(this.ball.body.velocity.x * 1.4);
         }
     }
 
@@ -31,6 +22,12 @@ window.onload = () => {
         type: Phaser.AUTO,
         width: 600,
         height: 600,
+        physics: {
+            default: 'arcade',
+            arcade: {
+                gravity: {y: 2000},
+            }
+        },
         backgroundColor: '#eeeeee',
         scene: [MainScene]
     });
